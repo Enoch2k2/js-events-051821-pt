f2a0f4f841dfde244818f54a5e9a57684168163d
Enoch2k2 <enoch2k2@hotmail.com>
2021-11-18 18:02:40 -0600

    finished demo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 051821 PT - JS - Events
+
+Overall: We want to fill out the form for the book. Click the create author button. Input will reset itself. A book item will appear under the book list.
+
+1. At what time do we want to be able to trigger this event? (DOMContentLoaded)
+
+2. What triggers this event? (cause) (submit)
+
+3. What is supposed to happen when the event is triggered. (effect) A book item will appear under the book list.
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,71 @@
-alert('hi')
+const books = [];
+
+/** NODE GETTERS **/
+const getForm = () => document.getElementById('form');
+const getTitle = () => document.getElementById('title');
+const getAuthor = () => document.getElementById('author');
+const getMain = () => document.getElementById('main')
+
+/** Templates **/
+
+const bookTemplate = book => {
+  // <div>
+  //     <h3>Book Title</h3>
+  //     <p>Author: Author Name</p>
+  //     <button>Click Me To Turn Text Blue</button>
+  //   </div>
+
+  const div = document.createElement('div');
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
+  const button = document.createElement('button');
+
+  h3.innerText = book.title;
+  p.innerText = `Author: ${ book.author }`;
+  button.innerText = "Click Me To Turn Text Blue";
+
+  button.addEventListener('click', changeToBlue);
+
+  div.appendChild(h3);
+  div.appendChild(p);
+  div.appendChild(button);
+
+  getMain().appendChild(div);
+}
+
+/** EventListeners **/
+// const attachEventToButton = () => {
+//   getButton().addEventListener('click', changeToBlue);
+// }
+
+const attachSubmitToForm = () => {
+  getForm().addEventListener('submit', createBook);
+}
+
+/** Event Handlers **/
+const changeToBlue = (event) => {
+  event.target.style.backgroundColor = 'blue'
+  event.target.style.color = 'white'
+}
+
+const createBook = (event) => {
+  event.preventDefault();
+  const bookObj = {
+    title: getTitle().value,
+    author: getAuthor().value
+  }
+  books.push(bookObj)
+
+  getTitle().value = ""
+  getAuthor().value = ""
+
+  bookTemplate(bookObj);
+}
+
+/** Events **/
+
+document.addEventListener('DOMContentLoaded', () => {
+  // attach events to nodes
+  // attachEventToButton();
+  attachSubmitToForm()
+})
